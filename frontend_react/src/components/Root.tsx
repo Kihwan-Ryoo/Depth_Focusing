fbc2f78a2c3821e39c793f8b315238655fd62029
SonJinHYo <wlsgy95@naver.com>
2023-01-31 16:35:28 +0900

    react-icon 설치, 헤더 생성

--- frontend_react/src/components/Root.tsx
+++ frontend_react/src/components/Root.tsx
@@ -1,10 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { HStack, Button, Box } from "@chakra-ui/react";
+import { Link, Outlet } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
 
 export default function Root() {
   return (
-    <h1>
-      im root
+    <Box>
+      <HStack
+        justifyContent={"space-between"}
+        py={5}
+        px={10}
+        borderBottomWidth={1}
+      >
+        <Box color="gray.900">
+          <Link to={"/"}>
+            <FaHome size={"48"} />
+          </Link>
+        </Box>
+        <HStack spacing={2}>
+          <Button>Log in</Button>
+          <Button colorScheme={"red"}>Sign up</Button>
+        </HStack>
+      </HStack>
       <Outlet />
-    </h1>
+    </Box>
   );
 }
