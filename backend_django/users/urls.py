c64261b080573a762466404bca141947d1bf1cff
SonJinHYo <wlsgy95@naver.com>
2023-01-31 13:50:16 +0900

    로그인 view 생성

--- backend_django/users/urls.py
+++ backend_django/users/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
+    path("log-in", views.LogIn.as_view()),
+    # path("log-out", views.Me.as_view()),
+    path("me", views.Me.as_view()),
 ]
--- backend_django/users/views.py
+++ backend_django/users/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import authenticate, login, logout
+
 from rest_framework.views import APIView
 from rest_framework import exceptions
 from rest_framework.response import Response
+from rest_framework import status
 
 from . import serializers
 
@@ -46,3 +49,25 @@ class Users(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.error)
+
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise exceptions.ParseError
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+
+        if user:
+            login(request, user)
+            return Response({"ok": "Welcome"}, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {"error": "wrong password"}, status=status.HTTP_400_BAD_REQUEST
+            )
