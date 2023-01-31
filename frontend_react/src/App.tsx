dded860254d8369f026e45803657677655d1b6f5
SonJinHYo <wlsgy95@naver.com>
2023-01-31 15:53:46 +0900

    frontend폴더에서 커밋테스트

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacttttt
+          Learn React
         </a>
       </header>
     </div>
