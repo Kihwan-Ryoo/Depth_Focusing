a36104964941fbdd7e2d2d29f225298dd82019ba
SonJinHYo <wlsgy95@naver.com>
2023-02-01 11:35:28 +0900

    프로필 사진 클릭 -> 로그아웃 기능 구현

--- frontend_react/src/components/Header.tsx
+++ frontend_react/src/components/Header.tsx
@@ -3,11 +3,18 @@ import {
   Avatar,
   Box,
   Button,
+  Flex,
   HStack,
   IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
   useColorMode,
   useColorModeValue,
   useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import LoginModal from "./LoginModal";
@@ -25,19 +32,44 @@ export default function Header() {
   const logoColor = useColorModeValue("gray.900", "gray.50");
   const Icon = useColorModeValue(FaMoon, FaSun);
 
+  const toast = useToast();
+  const onLogOut = async () => {
+    const toastId = toast({
+      title: "Login out",
+      status: "loading",
+      position: "bottom-right",
+    });
+    setTimeout(() => {
+      toast.update(toastId, {
+        status: "success",
+        title: "Done",
+        description: "Success Logout",
+      });
+    }, 5000);
+  };
+
   return (
-    <HStack
+    <Stack
+      align={"center"}
       justifyContent={"space-between"}
       py={5}
-      px={10}
+      px={40}
       borderBottomWidth={1}
+      direction={{
+        sm: "column",
+        md: "row",
+      }}
+      spacing={{
+        sm: 4,
+        md: 0,
+      }}
     >
       <Box color={logoColor}>
         <Link to={"/"}>
           <FaHome size={"48"} />
         </Link>
       </Box>
-      <HStack spacing={2}>
+      <Flex gap={2} alignItems="center">
         <IconButton
           onClick={toggleColorMode}
           variant={"ghost"}
@@ -48,11 +80,18 @@ export default function Header() {
           !isLoggedIn ? (
             <Button onClick={onLoginOpen}>Log in</Button>
           ) : (
-            <Avatar size={"md"} />
+            <Menu>
+              <MenuButton>
+                <Avatar name={user?.name} src={user?.avatar} size={"md"} />
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={onLogOut}>Log out</MenuItem>
+              </MenuList>
+            </Menu>
           )
         ) : null}
-      </HStack>
+      </Flex>
       <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
-    </HStack>
+    </Stack>
   );
 }
